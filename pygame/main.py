f362649f0b531b686fe191413e7f57c153eec974
Knugen (kactuz) <viktor.asp2@elev.ga.ntig.se>
2022-03-27 14:34:25 +0200

    Update main.py

--- pygame/main.py
+++ pygame/main.py
@@ -5,39 +5,50 @@ import random
 from sys import exit
 import numpy as np
 import random
- 
-fps_speed = 15
-window_x, window_y = pygame.display.set_mode().get_size()
 
-print("Window size:",window_x,"X",window_y)
+#_____________________________________________________________________________#
+#                                                                             #
+#--------------------------   CHANGEABLE SETTINGS   --------------------------#
+#_____________________________________________________________________________#
+fps_speed = 60
 
-# defining colors
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(255, 0, 0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
- 
-# Initialising pygame
+
+shoot_delay = 10
+bullet_speed = 3
+player_speed = 2
+enemy_speed = 2
+maximum_enemys = 100
+spawnChance = 1 #Works as procent up to 100. Tries to spawn each render cycle
+
+fullscreen = False
+window_x, window_y = (600,600)
+#------------------------------------------------------------------------------#
+
+if fullscreen: 
+    window_x, window_y = pygame.display.set_mode().get_size()
+    game_window = pygame.display.set_mode((window_x, window_y))
+else: game_window = pygame.display.set_mode((window_x, window_y), pygame.RESIZABLE)
+
+up    = False
+down  = False
+right = False
+left  = False
+
+bullet_list=[]
+enemy_list=[]
+small_window_x, small_window_y = window_x, window_y
 pygame.init()
- 
-# Initialise game window
 pygame.display.set_caption('Ett spel, I guess')
-
-game_window = pygame.display.set_mode((0, 0))
-
 fps = pygame.time.Clock()
 
-shoot_delay_time = 10
-shoot_delay = 10
 
-bullet_speed = 20
 
-direction = 'RIGHT'
-change_to = direction
-
-body_pos = [100, 800]
-body = [[100, 50]]
+print("Window size:",window_x,"X",window_y)
 
 def randId(array,IdPos):
     Id = random.randrange(1,10000)
@@ -70,234 +81,236 @@ def existsInArray(refrenceValue,searchArray,searchIndex):
         if refrenceValue == search[searchIndex]:
             retVal = True
     return retVal
-def countArray(array):
-    i=0
-    for j in array:
-        i+=1
-    return i
-def render():
-    player.renderPlayer()
-    enemy.renderEnemy()
-    tank_bullet.renderBullet()
-def moveObjects():
-    enemy.moveEnemy()
-    tank_bullet.move_bullet()
+def playerOutside():
+    if player.y > window_y - 30:
+        player.y = window_y - 30
+    if player.x > window_x - 30:
+        player.x = window_x - 30
+def moveEntity():
+    if len(bullet_list) != 0: 
+        for i in bullet_list:
+            i.move()
+    if len(enemy_list) != 0:
+        for ii in enemy_list:
+            ii.move()
+def renderEntity():
+    if len(bullet_list) != 0:
+        for j in bullet_list:
+            j.render()
+    if len(enemy_list) != 0:
+        for jj in enemy_list:
+            jj.render()
 
 
-class enemy:
-    enemys = []
 
-    def __init__(self,enemys):
-        self.enemys = enemys
+class playerClass():
+    def __init__(self):
+        self.x = round(window_x/2)
+        self.y = round(window_y/2)
+        self.canon_direction = [True,True]
 
-    def spawn(timer):
-        if timer:
-            if random.randrange(0,20) == 0:
-                spawnSpawn()
-            else:
-                spawnSpawn()
+    def move(self,direction):
+        if direction == [False,False]: # Down
+            self.y+=player_speed
+        elif direction == [True,True]: # Up
+            self.y-=player_speed
+        elif direction == [False,True]: # Right
+            self.x-=player_speed
+        elif direction == [True,False]: # Left
+            self.x+=player_speed
 
-            def __spawnSpawn():
-                direction = [trueFalse(),trueFalse()] #x,y starting from 11:up 10:left 01:right 00:down
-                Id = randId(enemy.enemys, 2)
+    def render(self):
+        pygame.draw.rect(game_window, white, pygame.Rect(self.x, self.y, 30, 30))
+        if self.canon_direction == [False,False]: # Down
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+10, self.y+30, 10, 10))
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+10, self.y+40, 10, 10))
+        elif self.canon_direction == [True,True]: # Up
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+10, self.y-10, 10, 10))
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+10, self.y-20, 10, 10))
+        elif self.canon_direction == [False,True]: # Right
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+30, self.y+10, 10, 10))
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x+40, self.y+10, 10, 10))
+        elif self.canon_direction == [True,False]: # Left
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x-10, self.y+10, 10, 10))
+            pygame.draw.rect(game_window, blue, pygame.Rect(self.x-20, self.y+10, 10, 10))
+player = playerClass()
 
-                newPosX = random.randrange(0,window_x-10)
-                newPosY = random.randrange(0,window_y-10)
+class bullet():
+    def __init__(self):
+        if player.canon_direction == [False,False]: # Down
+            self.x = player.x+10
+            self.y = player.y+50
+            self.direction = [False,False]
+        elif player.canon_direction == [True,True]: # Up
+            self.x = player.x+10
+            self.y = player.y-30
+            self.direction = [True,True]
+        elif player.canon_direction == [False,True]: # Right
+            self.x = player.x+50
+            self.y = player.y+10
+            self.direction = [False,True]
+        elif player.canon_direction == [True,False]: # Left
+            self.x = player.x-30
+            self.y = player.y+10
+            self.direction = [True,False]
 
-                if direction == [True,True]:
-                    enemy.enemys.append([newPosX,0,direction,Id])
-                elif direction == [True,False]:
-                    enemy.enemys.append([0,newPosY,direction,Id])
-                elif direction == [False,False]:
-                    enemy.enemys.append([newPosX,window_y,direction,Id])
-                elif direction == [False,True]:
-                    enemy.enemys.append([window_x,newPosY,direction,Id])
-    
-    def moveEnemy():
-        if len(enemy.enemys) != 0:
-            for theEnemy in enemy.enemys:
-                index = findIndex(theEnemy, enemy.enemys)
-                if theEnemy[2] == [True,True]:
-                    enemy.enemys[index][1]+=10
-                    if enemy.enemys[index][1] < 0:
-                        enemy.enemys.remove(enemy.enemys[index])
-                elif theEnemy[2] == [True,False]:
-                    enemy.enemys[index][0]+=10
-                    if enemy.enemys[index][1] < 0:
-                        enemy.enemys.remove(enemy.enemys[index])
-                elif theEnemy[2] == [False,False]:
-                    enemy.enemys[index][1]-=10
-                    if enemy.enemys[index][1] > window_x:
-                        enemy.enemys.remove(enemy.enemys[index])
-                elif theEnemy[2] == [False,True]:
-                    enemy.enemys[index][0]-=10
-                    if enemy.enemys[index][1] > window_y:
-                        enemy.enemys.remove(enemy.enemys[index])
+    def spawn():
+        bullet_list.append(bullet())
 
-    def renderEnemy():
-        for theEnemy in enemy.enemys:
-            pygame.draw.rect(game_window, blue, pygame.Rect(theEnemy[0], theEnemy[1], 20, 20))
+    def move(self):
+        if self.x < 0 or self.x > window_x or self.y < 0 or self.y > window_y:
+            bullet_list.remove(self)
 
-class player:
-    up = False
-    down = False
-    left = False
-    right = False
-    canon_direction = [False,False] #x,y facing the canon 11:up 10:left 01:right 00:down
+        if self.direction == [False,False]: # Down
+            self.y += bullet_speed
+        elif self.direction == [True,True]: # Up
+            self.y -= bullet_speed
+        elif self.direction == [False,True]: # Right
+            self.x += bullet_speed
+        elif self.direction == [True,False]: # Left
+            self.x -= bullet_speed
 
-    def __init__(self,up,down,left,right,canon_direction):
-        self.up = up
-        self.down = down
-        self.left = left
-        self.right = right
-        self.canon_direction = canon_direction
-        # self.tankBullet = self.tank_bullet()
-    
-    def renderPlayer():
-        pygame.draw.rect(game_window, green, pygame.Rect(body_pos[0], body_pos[1], 30, 30))
-            
-        if player.canon_direction==[True,True]:
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+10, body_pos[1]-10, 10, 10))
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+10, body_pos[1]-20, 10, 10))
-        elif player.canon_direction==[True,False]:
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]-10, body_pos[1]+10, 10, 10))
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]-20, body_pos[1]+10, 10, 10))
-        elif player.canon_direction==[False,True]:
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+30, body_pos[1]+10, 10, 10))
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+40, body_pos[1]+10, 10, 10))
-        elif player.canon_direction==[False,False]:
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+10, body_pos[1]+30, 10, 10))
-            pygame.draw.rect(game_window, blue, pygame.Rect(body_pos[0]+10, body_pos[1]+40, 10, 10))
+    def render(self):
+        pygame.draw.rect(game_window, blue, pygame.Rect(self.x, self.y, 10, 10))
 
-    def input():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+class enemy():
+    def __init__(self):
+        i = random.randrange(0,4)
+        jx = random.randrange(0,window_x)
+        jy = random.randrange(0,window_y)
+        print(i,jx,jy)
+
+        if i == 0: # Down
+            self.x = jx
+            self.y = 30
+            self.direction = [False,False]
+        elif i == 1: # Up
+            self.x = jx
+            self.y = window_y-30
+            self.direction = [True,True]
+        elif i == 2: # Right
+            self.x = 30
+            self.y = jy
+            self.direction = [False,True]
+        elif i == 3: # Left
+            self.x = window_x-30
+            self.y = jy
+            self.direction = [True,False]  
+
+    def spawn():
+        if random.randrange(0,100) < spawnChance:
+            enemy_list.append(enemy())
+
+    def move(self):
+        if self.x < -30 or self.x > window_x or self.y < -30 or self.y > window_y:
+            enemy_list.remove(self)
+
+        if self.direction == [False,False]: # Down
+            self.y += enemy_speed
+        elif self.direction == [True,True]: # Up
+            self.y -= enemy_speed
+        elif self.direction == [False,True]: # Right
+            self.x += enemy_speed
+        elif self.direction == [True,False]: # Left
+            self.x -= enemy_speed
+
+    def render(self):
+        pygame.draw.rect(game_window, green, pygame.Rect(self.x, self.y, 30, 30))
+
+    def detectCollision(self):
+        for bullet in bullet_list:
+            x1,x2 = bullet.x,bullet.x+10
+            y1,y2 = bullet.y,bullet.y+10
+            xx,yy = range(self.x,self.x+30),range(self.y,self.y+30)
+
+
+            if x1 in xx or x2 in xx:
+                if y1 in yy or y2 in yy:
+                    print("Hit")
+                    try:
+                        enemy_list.remove(self)
+                    except:
+                        print("Can't kill")
+def input():
+    global up,down,right,left,window_x,window_y,fullscreen,last_window_x,last_window_y,small_window_x, small_window_y
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w: up   =  True
+            if event.key == pygame.K_s: down =  True
+            if event.key == pygame.K_a: right=  True
+            if event.key == pygame.K_d: left =  True
+
+            if event.key == pygame.K_UP: player.canon_direction   = [True,True]
+            if event.key == pygame.K_DOWN: player.canon_direction = [False,False]
+            if event.key == pygame.K_LEFT: player.canon_direction = [True,False]
+            if event.key == pygame.K_RIGHT: player.canon_direction= [False,True]
+
+            if event.key == pygame.K_ESCAPE or event.key == pygame.K_r:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    player.up = True
-                if event.key == pygame.K_s:
-                    player.down = True
-                if event.key == pygame.K_a:
-                    player.left = True
-                if event.key == pygame.K_d:
-                    player.right = True
+            if event.key == pygame.K_SPACE:
+                bullet.spawn()
 
-                if event.key == pygame.K_UP:
-                    player.canon_direction=[True,True]
-                if event.key == pygame.K_DOWN:
-                    player.canon_direction=[False,False]
-                if event.key == pygame.K_LEFT:
-                    player.canon_direction=[True,False]
-                if event.key == pygame.K_RIGHT:
-                    player.canon_direction=[False,True]
 
-                if event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    quit()
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w: up   =  False
+            if event.key == pygame.K_s: down =  False
+            if event.key == pygame.K_a: right=  False
+            if event.key == pygame.K_d: left =  False
 
-                if event.key == pygame.K_SPACE:
-                    if shoot_delay == 0:
-                        tank_bullet.shoot()
-                if event.key == pygame.K_r:
-                    enemy.enemys=[[100,900,[True,True],1]]
+            if event.key == pygame.K_l:
+                enemy_list.append(enemy())
+            if event.key == pygame.K_F11: 
+                print("Screen Size: ", pygame.display.set_mode().get_size())
+                print("Last Size: ", small_window_x, small_window_y)
+                if fullscreen == False:
+                    fullscreen=True
+                    game_window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                    window_x, window_y = pygame.display.get_window_size()
+                    playerOutside()
+                else: 
+                    fullscreen=False
+                    game_window = pygame.display.set_mode((small_window_x,small_window_y), pygame.RESIZABLE)
+                    window_x, window_y = pygame.display.get_window_size()
+                    small_window_x,small_window_y = window_x, window_y
+                    playerOutside()
 
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_w:
-                    player.up = False
-                if event.key == pygame.K_s:
-                    player.down = False
-                if event.key == pygame.K_a:
-                    player.left = False
-                if event.key == pygame.K_d:
-                    player.right = False
 
-    def player_movement():
-        if player.up:
-            if body_pos[1] > 0:
-                body_pos[1] -= 10
-        if player.down:
-            if body_pos[1] < window_y - 30:
-                body_pos[1] += 10
-        if player.left:
-            if body_pos[0] > 0:
-                body_pos[0] -= 10
-        if player.right:
-            if body_pos[0] < window_x - 30:
-                body_pos[0] += 10
-    
-class tank_bullet:
-    direction = [False,False] #x,y facing the canon 11:up 10:left 01:right 00:down
-    bullets = []
+        if event.type == pygame.VIDEORESIZE:
+            window_x, window_y = pygame.display.get_window_size()
+            if fullscreen == False: small_window_x,small_window_y = window_x, window_y
+            playerOutside()
 
-    def __init__(self,direction,bullets):
-        self.direction = directionv
-        self.bullets = bullets
-
-    def shoot():
-        global shoot_delay
-        global shoot_delay_time
-        shoot_delay=shoot_delay_time
-        Id = randId(tank_bullet.bullets,3)
-    
-        if player.canon_direction==[True,True]:
-            tank_bullet.bullets.append([body_pos[0]+10, body_pos[1]-30, [True,True], Id])
-        elif player.canon_direction==[True,False]:
-            tank_bullet.bullets.append([body_pos[0]-30, body_pos[1]+10, [True,False], Id])
-        elif player.canon_direction==[False,True]:
-            tank_bullet.bullets.append([body_pos[0]+50, body_pos[1]+10, [False,True], Id])
-        elif player.canon_direction==[False,False]:
-            tank_bullet.bullets.append([body_pos[0]+10, body_pos[1]+50, [False,False], Id])
-    
-    def renderBullet():
-        if len(tank_bullet.bullets) != 0:
-            for bullet in tank_bullet.bullets:
-                pygame.draw.rect(game_window, white, pygame.Rect(bullet[0],bullet[1], 10, 10))
-
-    def move_bullet():
-        if len(tank_bullet.bullets) != 0:
-            for bullet in tank_bullet.bullets:
-                index = findIndex(bullet, tank_bullet.bullets)
-
-                if bullet[2]==[True,True]:
-                    tank_bullet.bullets[index][1]-=bullet_speed
-                    if tank_bullet.bullets[index][1] < 0:
-                        tank_bullet.bullets.remove(tank_bullet.bullets[index])
-                elif bullet[2]==[True,False]:
-                    tank_bullet.bullets[index][0]-=bullet_speed
-                    if tank_bullet.bullets[index][1] < 0:
-                        tank_bullet.bullets.remove(tank_bullet.bullets[index])
-                elif bullet[2]==[False,True]:
-                    tank_bullet.bullets[index][0]+=bullet_speed
-                    if tank_bullet.bullets[index][1] > window_x:
-                        tank_bullet.bullets.remove(tank_bullet.bullets[index])
-                elif bullet[2]==[False,False]:
-                    tank_bullet.bullets[index][1]+=bullet_speed
-                    if tank_bullet.bullets[index][1] > window_y:
-                        tank_bullet.bullets.remove(tank_bullet.bullets[index])
-
-# tank_bullet.bullets=[[100,900,[True,True],2]]
-# enemy.enemys=[[100,900,[True,True],1]]
+    if up == True:
+        if player.y > 0:
+            player.move([True,True])
+    if down == True:
+        if player.y < window_y - 30:
+            player.move([False,False])
+    if left == True:
+        if player.x < window_x - 30:
+            player.move([True,False])
+    if right == True:
+        if player.x > 0:
+            player.move([False,True])
 
 while True:
-    player.input()
-    player.player_movement()
-    
-    body.pop()
-    body.insert(0, list(body_pos))
+    input()
+    enemy.spawn() 
+
+    moveEntity()
+
+    for i in enemy_list:
+        i.detectCollision()
 
     game_window.fill(black)
-
-    # enemy.spawn(True)
-
-    # checkCollision(tank_bullet.bullets, enemy.enemys)
-
-    render()
-    moveObjects()
-
-    if shoot_delay != 0:
-        shoot_delay-=1
+    player.render()
+    renderEntity()
 
     pygame.display.update()
     fps.tick(fps_speed)
